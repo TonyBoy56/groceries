7eceb75b951b33b05dcfc705f391b719f5e1a014
Student <example@example.com>
2020-01-21 16:48:12 -0800

    add working grocery page. May need adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-// Front-end logic //
-
 $(document).ready(function() {
-    $("#list").submit(function(event) {
-        $("#list").toggle();
+    $("#input").submit(function(event) {
         var result1 = $("input#groclist").val();
         $("#things").show();
         $("#things").append("<li>" + result1+ "</li>");
-
         event.preventDefault();
+    });
 
-
+    $("#list").submit(function(event) {
+        // var result2 = result1;
+        $("#input").hide();
+      //  $("#show").append("<li>" + result2+ "</li>");
+        event.preventDefault();
     });
     
 });
-
-// Back-end logic //
-
     
